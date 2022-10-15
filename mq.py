d82e667201b8dcb0780e5bde92dbe7701a3fbe38
silwalgg <108857433+silwalgg@users.noreply.github.com>
2022-10-15 10:40:34 +0545

    Gets status from db before running

--- mq.py
+++ mq.py
@@ -2,19 +2,24 @@
 import requests
 from beebotte import *
 
-
+import streamlit as st
 _accesskey  = "49VqJ9vAGYrvFlqgILiSLeDN"
 _secretkey  = "KUhmrP1mm2w57tVVvSUYf6tPqN2Tu5J9"
 _hostname   = 'api.beebotte.com'
 bbt = BBT( _accesskey, _secretkey, hostname = _hostname)
 
+records = bbt.read('Arduino','led',limit = 5)
+if records[0]['data']:
+	value='ON'
+else:
+	value='OFF'
 
-bbt.write('Arduino','led',True)
 
-import streamlit as st
+
 st.title('Lights Control')
-lit= st.select_slider('Lights',     options=['OFF','ON'])
+lit= st.select_slider('Lights',     options=['OFF','ON'],value=value)
 if lit=='ON':
 	bbt.write('Arduino','led',True)
 elif lit=='OFF':
 	bbt.write('Arduino','led',False)
+#hello
